9f1c793398e9a9b68199695bbf893520c9d5b5e2
michelly.nonato <michelly.nonato@ifood.com.br>
2024-04-22 14:49:53 -0300

    Day 3: Multi-line Strings

--- playGround/MyPlayground.playground/Contents.swift
+++ playGround/MyPlayground.playground/Contents.swift
@@ -9,6 +9,8 @@ var favoriteShow = "Anne With an E"
 favoriteShow = "The Office"
 favoriteShow = "Bridgerton"
 
+
+
 // Dia 2: Strings e Inteiros
 
 var age = 21
@@ -19,3 +21,22 @@ var population = 8_000_000_000 // 8000000000
 // Variáveis podem ser modificadas em qualquer momento do códigos, porém o seu tipo nunca é alterado
 var meaningOfLife = 42
 // <error> meaningOfLife = "Forty two"
+
+
+
+
+// Dia 3: String com múltiplas linhas
+
+var str1 = """
+This goes
+over multiple
+lines
+""" // Com quebra de linhas
+
+var str2 = """
+This goes \
+over multiple \
+lines 
+""" // Sem quebra de linhas
+
+// Multilinhas não é utilizado em mensagens de erro, pois pode ocorrer falha durante a pesquisa da mensagem.

c04f00e81fbe4e2d1bcc3aae7911cab0aa8184a2
MichellyNonatto <smartnovo01@gmail.com>
2024-05-03 10:25:56 -0300

    Improve the slider

--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -7,26 +7,29 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
-    var currentValue: Int = 50
+    @IBOutlet var slider: UISlider!
+    var currentValue: Int?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        currentValue = lroundf(slider.value)
     }
     
     /// Cria um pop-up ao clicar no botão `Hit Me`
     @IBAction func showAlert() {
-        let message = "The value of the slider is: \(currentValue)"
+        let message = "The value of the slider is: \(currentValue!)"
         let alert = UIAlertController(
-        title: "Hello World",
-        message: message,
-        preferredStyle: .alert
+            title: "Hello World",
+            message: message,
+            preferredStyle: .alert
         )
         
         let action = UIAlertAction(
-        title: "OK",
-        style: .default
+            title: "OK",
+            style: .default
         )
         
         alert.addAction(action)
@@ -42,20 +45,20 @@ class ViewController: UIViewController {
     /// Cria um pop-up ao clicar no botão de informação
     @IBAction func infoAlert() {
         let alert = UIAlertController(
-        title: "Informações",
-        message: "Este jogo é para você adivinhar em qual número estou pensando.\nVamos lá, tente a sorte!",
-        preferredStyle: .alert
+            title: "Informações",
+            message: "Este jogo é para você adivinhar em qual número estou pensando.\nVamos lá, tente a sorte!",
+            preferredStyle: .alert
         )
         
         let action = UIAlertAction(
-        title: "Voltar",
-        style: .default
+            title: "Voltar",
+            style: .default
         )
         
         alert.addAction(action)
         present(alert, animated: true)
     }
     
-
+    
 }
 

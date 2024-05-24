01ced7fe2cea4b547adcf6803817ad6e8c708de9
MichellyNonatto <smartnovo01@gmail.com>
2024-05-24 14:58:49 -0300

    Learning TableUI

---
+++ LearningTableUI/LearningTableUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LearningTableUI
+//
+//  Created by Michelly Nonato on 24/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

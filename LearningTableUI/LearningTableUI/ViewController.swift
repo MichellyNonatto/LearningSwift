39b7d9b19473a9b4bcb12c434e91a74c38f0ba22
Michelly Nonato <michelly.nonato@ifood.com.br>
2024-06-07 15:50:44 -0300

    Learning UITableView with storyboard

--- LearningTableUI/LearningTableUI/ViewController.swift
+++ LearningTableUI/LearningTableUI/ViewController.swift
@@ -6,14 +6,24 @@
 //
 
 import UIKit
-
 class ViewController: UIViewController {
+    @IBOutlet weak var myTable: UITableView!
+
+    var youtubeChannels = ["Daniel Pinheiro", "Tiago Santineli", "Felca"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return youtubeChannels.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = myTable.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = youtubeChannels[indexPath.row]
+        return cell
+    }
+}
